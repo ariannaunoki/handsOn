c6d0b57a0854301b60897dcc0bd94e670f14a0aa
Arianna Unoki <unokiarianna@gmail.com>
2023-07-18 13:17:16 -0700

    Initial Commit

---
+++ handsOn/handsOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  handsOnApp.swift
+//  handsOn
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct handsOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
